3fd42228536c12229ce7c7a423ef6b284bc015a6
Everton Pila Silva <84790670+Everton-Pila-Silva@users.noreply.github.com>
2022-02-01 18:12:13 -0300

    Update tensor.js

--- tensor.js
+++ tensor.js
@@ -50,22 +50,22 @@
                 prediction[i].className + ": " + prediction[i].probability.toFixed(2);
             labelContainer.childNodes[i].innerHTML = classPrediction;
              
-              if(prediction[i].className = '2 Reais' && prediction[i].probability == 0.95 && j == 0){
+              if(prediction[i].className = '2 Reais' && prediction[i].probability > 0.99  && j == 0){
                 const music = new Audio('2reais.mp3');
                 music.play();
                 j = 1;
              }
-             if(prediction[i].className = '10 Reais' && prediction[i].probability == 0.95  && j == 0){
+             if(prediction[i].className = '10 Reais' && prediction[i].probability > 0.99  && j == 0){
                 const music = new Audio('10reais.mp3');
                 music.play();
                j = 1;
             }
-              if(prediction[i].className = '5 Reais' && prediction[i].probability == 0.95 && j == 0){
+              if(prediction[i].className = '5 Reais' && prediction[i].probability > 0.99  && j == 0){
                 const music = new Audio('5reais.mp3');
                 music.play();
                 j = 1;
             }
-               if(prediction[i].className = '50 Reais' && prediction[i].probability == 0.95 && j == 0 ){
+               if(prediction[i].className = '50 Reais' && prediction[i].probability > 0.99  && j == 0 ){
                 const music = new Audio('50reais.mp3');
                 music.play();
               j = 1;
