5c7ca4933362bd28d440295eb9f3c29e5b24094b
Everton Pila Silva <84790670+Everton-Pila-Silva@users.noreply.github.com>
2022-02-01 17:45:10 -0300

    Update tensor.js

--- tensor.js
+++ tensor.js
@@ -50,32 +50,32 @@
                 prediction[i].className + ": " + prediction[i].probability.toFixed(2);
             labelContainer.childNodes[i].innerHTML = classPrediction;
              
-               if(prediction[i].className == '2 Reais' & prediction[i].probability == 0.94 ){
+              if(prediction[i].className = '2 Reais' & prediction[i].probability == 0.94 ){
                 const music = new Audio('2reais.mp3');
                 music.play();
               j ++;
              }
-             if(prediction[i].className == '10 Reais'  & prediction[i].probability == 0.94){
+             if(prediction[i].className = '10 Reais'  & prediction[i].probability == 0.94){
                 const music = new Audio('10reais.mp3');
                 music.play();
                j ++;
             }
-              if(prediction[i].className == '5 Reais'  & prediction[i].probability == 0.94){
+              if(prediction[i].className = '5 Reais'  & prediction[i].probability == 0.94){
                 const music = new Audio('5reais.mp3');
                 music.play();
                j ++;
             }
-               if(prediction[i].className == '50 Reais'  & prediction[i].probability == 0.94){
+               if(prediction[i].className = '50 Reais'  & prediction[i].probability == 0.94){
                 const music = new Audio('50reais.mp3');
                 music.play();
                 j ++;
             }
+             
          }else{
             labelContainer.childNodes[i].innerHTML = "-";
          
         }  
-            
-         
+     
       
         }
   
